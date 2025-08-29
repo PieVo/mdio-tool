4a93fae9d21b18bbdcd9ab7b148db81ed865c58e
Sebastian Pastor <sebastianpastor@gmail.com>
2025-08-29 07:55:41 +0000

    mdio-tool: add methods to read and write extended registers
    Expand the help message and README.md a little bit.

--- mdio-tool.c
+++ mdio-tool.c
@@ -50,35 +50,59 @@ static struct ifreq ifr;
 
 static int mdio_read(int skfd, int location)
 {
-    struct mii_data *mii = (struct mii_data *)&ifr.ifr_data;
-    mii->reg_num = location;
-    if (ioctl(skfd, SIOCGMIIREG, &ifr) < 0) {
-	fprintf(stderr, "SIOCGMIIREG on %s failed: %s\n", ifr.ifr_name,
-		strerror(errno));
-	return -1;
-    }
-    return mii->val_out;
+	struct mii_data *mii = (struct mii_data *)&ifr.ifr_data;
+	mii->reg_num = location;
+	if (ioctl(skfd, SIOCGMIIREG, &ifr) < 0) {
+		fprintf(stderr, "SIOCGMIIREG on %s failed: %s\n", ifr.ifr_name,
+			strerror(errno));
+		return -1;
+	}
+	return mii->val_out;
 }
 
 static void mdio_write(int skfd, int location, int value)
 {
-    struct mii_data *mii = (struct mii_data *)&ifr.ifr_data;
-    mii->reg_num = location;
-    mii->val_in = value;
-    if (ioctl(skfd, SIOCSMIIREG, &ifr) < 0) {
-	fprintf(stderr, "SIOCSMIIREG on %s failed: %s\n", ifr.ifr_name,
-		strerror(errno));
-    }
+	struct mii_data *mii = (struct mii_data *)&ifr.ifr_data;
+	mii->reg_num = location;
+	mii->val_in = value;
+	if (ioctl(skfd, SIOCSMIIREG, &ifr) < 0) {
+		fprintf(stderr, "SIOCSMIIREG on %s failed: %s\n", ifr.ifr_name,
+			strerror(errno));
+	}
 }
 
+static int mdio_read_extended_reg(int skfd, int mmd, int location)
+{
+	mdio_write(skfd, MII_REGCR, mmd);
+	mdio_write(skfd, MII_ADDAR, location);
+	mdio_write(skfd, MII_REGCR, mmd | MII_ADDAR_DATA_NOINC);
+	return mdio_read(skfd, MII_ADDAR);
+}
+
+static void mdio_write_extended_reg(int skfd, int mmd, int location, int value)
+{
+	mdio_write(skfd, MII_REGCR, mmd);
+	mdio_write(skfd, MII_ADDAR, location);
+	mdio_write(skfd, MII_REGCR, mmd | MII_ADDAR_DATA_NOINC);
+	mdio_write(skfd, MII_ADDAR, value);
+}
 
 int main(int argc, char **argv)
 {
-	int addr, dev, val;
+	int addr, dev, val, mmd;
 	struct mii_data *mii = (struct mii_data *)&ifr.ifr_data;
 
 	if(argc < 2) {
-		printf("Usage mii [r/w] [dev] [reg] [val]\n");
+		printf("Usage:\nmdio-tool [r/w/er/ew] <dev> [mmd] <reg> [val]\n"
+			"\tOperation:\n"
+			"\t  r: read register\n"
+			"\t  w: write register\n"
+			"\t  er: read register from extended reg space\n"
+			"\t  ew: write register from extended reg space\n"
+			"\tdev: as in 'eth0'\n"
+			"\tmmd: for 'er' and 'ew' operations, specify the MDIO's Manageable Device\n"
+			"\treg: the register's address to read from/write to\n"
+			"\tval: the value to write in the register\n");
 		return 0;
 	}
 
@@ -92,12 +116,28 @@ int main(int argc, char **argv)
 	strncpy(ifr.ifr_name, argv[2], IFNAMSIZ);
 	if (ioctl(skfd, SIOCGMIIPHY, &ifr) < 0) {
 		if (errno != ENODEV)
-		fprintf(stderr, "SIOCGMIIPHY on '%s' failed: %s\n",
-			argv[2], strerror(errno));
+			fprintf(stderr, "SIOCGMIIPHY on '%s' failed: %s\n",
+					argv[2], strerror(errno));
 		return -1;
 	}
 
-	if(argv[1][0] == 'r') {
+	if(argv[1][0] == 'e') {
+		if(argv[1][1] == 'r') {
+			mmd = strtol(argv[3], NULL, 16);
+			addr = strtol(argv[4], NULL, 16);
+			printf("0x%.4x\n", mdio_read_extended_reg(skfd, mmd, addr));
+		}
+		else if(argv[1][1] == 'w') {
+			mmd = strtol(argv[3], NULL, 16);
+			addr = strtol(argv[4], NULL, 16);
+			val = strtol(argv[5], NULL, 16);
+			mdio_write_extended_reg(skfd, mmd, addr, val);
+		}
+		else {
+			printf("Fout!\n");
+		}
+	}
+	else if(argv[1][0] == 'r') {
 		addr = strtol(argv[3], NULL, 16);
 		printf("0x%.4x\n", mdio_read(skfd, addr));
 	}
